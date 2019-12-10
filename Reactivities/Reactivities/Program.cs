9cf35a4d7a0df0d1fe2bcd5cc3840326dfe711e4
Ali Khorsand <ali.khorsand@live.com>
2019-12-10 23:46:51 +0330

    Initial Api Setup

---
+++ Reactivities/Reactivities/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Reactivities
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // we change default options
+            // this change checks if database existed just run the application
+            // if not this will run update-database and will create one
+            var host = CreateWebHostBuilder(args).Build();
+            // with this line we can reach to all existing services scope and then can use them
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    // will get the datacontext service and will migrate the database
+                    var context = services.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    // reach out to logger service and then log something
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "an error occured during migration");
+                }
+            }
+            // after doing some initialization this wll run the host
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
